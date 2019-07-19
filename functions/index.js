0e5a3d6daaedd3bebf3f264a528af2fecf26e356
vuvo <vanvu2912@gmail.com>
2019-07-19 16:56:53 +0700

    first commit

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+//Get trigger when a user signs in for the first time using a federated identity provider.
+exports.addAccount = functions.auth.user().onCreate(user => {
+    var id = user.uid;
+    var db = admin.database();
+    var refUser = db.ref("user");
+    var refRecord = db.ref("record");
+    var newUser = {};
+    newUser[id] = {
+        managerid: '',
+        permission: ''
+    }
+    var newUserRecord = {};
+    newUserRecord[id] = {};
+    refUser.update(newUser);
+    refRecord.update(newUserRecord);
+});
